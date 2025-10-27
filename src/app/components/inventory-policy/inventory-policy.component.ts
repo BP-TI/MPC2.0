58103298d7f79e48fb2326ac8036d62218a7d4b8
DanielDelgado <ddelgado@sigcomt.com>
2025-10-27 16:33:56 -0500

    tercer comit

--- src/app/components/inventory-policy/inventory-policy.component.ts
+++ src/app/components/inventory-policy/inventory-policy.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PurchasePlanningService } from '../../services/PurchasePlanning/purchasePlanning.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+interface dataPolitica {
+  ABC:string;
+  MaxAlmacen:string;
+  Total:string;
+  puntoVenta:string;
+}
 
 @Component({
   selector: 'app-inventory-policy',
@@ -6,6 +15,40 @@ import { Component } from '@angular/core';
   templateUrl: './inventory-policy.component.html',
   styleUrl: './inventory-policy.component.css'
 })
-export class InventoryPolicyComponent {
+export class InventoryPolicyComponent implements OnInit {
+
+  loading: boolean = false;
+  rowsLb: any[];
+
+  constructor(private purchaseService: PurchasePlanningService,) {
+
+  }
+
+  ngOnInit(){
+    this.cargarPoliticas();
+  }
+
+  cargarPoliticas() {
+    this.loading = true;
+    this.purchaseService.getPoliticas().subscribe(
+      (response) => {
+        console.log(response);
+        this.loading = false;
+        this.rowsLb = response;
+      },
+      (error: HttpErrorResponse) => {
+        this.loading = false;
+      }
+    );
+  }
+
+  private insertDataTable(data:dataPolitica[]): void {
+    let tabla: HTMLTableElement = <HTMLTableElement> document.getElementById("politicasinventario");
+    let tr= document.createElement('tr');
+    data.forEach((data:dataPolitica) =>{
+      let td= document.createElement('td');
+      // tabla.insert
+    });
+  }
 
 }
